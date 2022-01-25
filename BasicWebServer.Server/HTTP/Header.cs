754fc09e84ad1a7351a92d6c260ca28395642f44
KristiyanBandrov <97550267+KristiyanBandrov@users.noreply.github.com>
2022-01-25 22:49:34 +0200

    Add project files.

---
+++ BasicWebServer.Server/HTTP/Header.cs
@@ -0,0 +1,40 @@
+﻿using BasicWebServer.Server.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class Header
+    {
+        public const string ContentType = "Content-Type";
+        public const string ContentLength = "Content-Length";
+        public const string ContentDisposition = "Content-Disposition";
+        public const string Cookie = "Cookie";
+        public const string Date = "Date";
+        public const string Location = "Location";
+        public const string Server = "Server";
+        public const string SetCookie = "Set-Cookie";
+
+
+        public string Name { get; init; }
+        public string Value { get; set; }
+
+        
+
+        public Header(string name, string value)
+        {
+            Guard.AgainstNull(name, nameof(name));
+            Guard.AgainstNull(value, nameof(value));
+
+
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public override string ToString()
+            => $"{this.Name}: {this.Value}";
+    }
+}
