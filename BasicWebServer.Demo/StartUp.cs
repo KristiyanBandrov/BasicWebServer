25c4c68096b82ee20372988a8138fca5377f1304
KristiyanBandrov <97550267+KristiyanBandrov@users.noreply.github.com>
2022-01-29 16:30:26 +0200

    Finished with HomeController functionality

--- BasicWebServer.Demo/StartUp.cs
+++ BasicWebServer.Demo/StartUp.cs
@@ -16,124 +16,17 @@ namespace BasicWebServer.Demo
             => await new HttpServer(routes => routes
             .MapGet<HomeController>("/", c => c.Index())
             .MapGet<HomeController>("/Redirect", c => c.Redirect())
-            .MapGet<HomeController>("/HTML", c => c.Html()))
-            //.MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
-            //.MapGet<HomeController>("/Content", c => c.Content())
-            //.MapPost<HomeController>("/Content", c => c.DownloadContent)
-            //.MapGet<HomeController>("/Cookies", c => c.Cookies())
-            //.MapGet<HomeController>("/Session", c => c.Session));
+            .MapGet<HomeController>("/HTML", c => c.Html())
+            .MapPost<HomeController>("/HTML", c => c.HtmlFormPost())
+            .MapGet<HomeController>("/Content", c => c.Content())
+            .MapPost<HomeController>("/Content", c => c.DownloadContent())
+            .MapGet<HomeController>("/Cookies", c => c.Cookies())
+            .MapGet<HomeController>("/Session", c => c.Session()))
             //.MapGet("/Login", new HtmlResponse(StartUp.LoginForm))
             //.MapPost("/Login", new HtmlResponse("", StartUp.LoginAction))
             //.MapGet("/Logout", new HtmlResponse("", StartUp.LogoutAction))
             //.MapGet("/UserProfile", new HtmlResponse("", StartUp.GetUserDataAction)))
             .Start();
-
-        private static void AddFormDataAction(
-            Request request, Response response)
-        {
-            response.Body = "";
-
-            foreach (var (key, value) in request.Form)
-            {
-                response.Body += $"{key} - {value}";
-                response.Body += Environment.NewLine;
-            }
-        }
-
-        private static async Task<string> DownloadWebSiteContent(string url)
-        {
-            var httpClient = new HttpClient();
-            using (httpClient)
-            {
-                var response = await httpClient.GetAsync(url);
-
-                var html = await response.Content.ReadAsStringAsync();
-
-                return html.Substring(0, 2000);
-            }
-        }
-
-        private static async Task DownloadSitesAsTextFile(
-            string fileName, string[] urls)
-        {
-            var downloads = new List<Task<string>>();
-
-            foreach (var url in urls)
-            {
-                downloads.Add(DownloadWebSiteContent(url));
-            }
-
-            var responses = await Task.WhenAll(downloads);
-
-            var responsesString = string.Join(
-                Environment.NewLine + new String('-', 100), responses);
-
-            await File.WriteAllTextAsync(fileName, responsesString);
-        }
-
-        private static void AddCookiesAction(Request request, Response response)
-        {
-            bool requestHasCookies = request.Cookies
-                .Any(c => c.Name != Session.SessionCookieName);
-            var bodyText = "";
-
-            if (requestHasCookies)
-            {
-                var cookieText = new StringBuilder();
-
-                cookieText.AppendLine("<h1>Cookies</h1>");
-
-                cookieText
-                    .Append("<table border='1'><tr><th>Name</th><th>Value</th></tr>");
-
-                foreach (var cookie in request.Cookies)
-                {
-                    cookieText.Append("<tr>");
-                    cookieText
-                        .Append($"<td>{HttpUtility.HtmlEncode(cookie.Name)}</td>");
-                    cookieText
-                        .Append($"<td>{HttpUtility.HtmlEncode(cookie.Value)}</td>");
-                    cookieText.Append("</tr>");
-                }
-
-                cookieText.Append("</table>");
-
-                bodyText = cookieText.ToString();
-            }
-            else
-            {
-                bodyText = "<h1>Cookies set!</h1>";
-            }
-
-            if (!requestHasCookies)
-            {
-                response.Cookies.Add("My-Cookie", "My-Value");
-                response.Cookies.Add("My-Second-Cookie", "My-Second-Value");
-            }
-        }
-
-        private static void DisplaySessionInfoAction
-            (Request request, Response response)
-        {
-            var sessionExists = request.Session.ContainsKey(Session.SessionCurrentDateKey);
-
-            var bodyText = "";
-
-            if (sessionExists)
-            {
-                var currentDate = request.Session[Session.SessionCurrentDateKey];
-                bodyText = $"Stored date: {currentDate}!";
-            }
-
-            else
-            {
-                bodyText = "Current date stored!";
-            }
-
-            response.Body = "";
-
-            response.Body += bodyText;
-        }
 
         private static void LoginAction(Request request, Response response)
         {
